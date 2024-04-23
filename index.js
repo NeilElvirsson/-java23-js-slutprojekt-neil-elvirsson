c43cd56995352b651f82a1d99f35c37b7932de4e
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2024-04-23 18:51:03 +0200

    added new update for top rated movies and tv show

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ console.log("Hello World!");
 
 const API_KEY = "9626218391ae38329b6d11901b20a32e";
 
-const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
+//const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
 
 //Gets our referenses to the DOM-element.
-const moviesContainer = document.getElementById("movies");
+const moviesContainer = document.getElementById("moviesContainer");
 const searchForm = document.getElementById("searchForm");
 
 //Adds eventlistener to our form
@@ -13,29 +13,93 @@ searchForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const searchTerm = document.getElementById("search").value.trim();
-  //If searchterm is not empty sends a request to the tmdb API
-  if (searchTerm !== '') {
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(searchTerm)}`;
+  const selectedCategory = document.querySelector('input[name="category"]:checked').value;
+  let searchUrl;
 
-    //We do a get-request to our url and convert the answer to json-format
-    fetch(searchUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        //console.log(json))
-        moviesContainer.innerHTML = '';
 
-        //Loops trough our filmobject from data results and creates a moviecard which we put in our movieconatiner
+
+  //If searchterm is empty and radio button movies or action is selected show trending
+  if (searchTerm === '' && selectedCategory === "movies") {
+    searchUrl = `https://api.themoviedb.org/3//movie/top_rated?api_key=${API_KEY}`;
+  }
+    else if (searchTerm === '' && selectedCategory === "tv_shows") {
+      searchUrl = `https://api.themoviedb.org/3/trending/tv/week?api_key=${API_KEY}`;
+    
+      //Else if a searchterm is put in and radio button is choosen, show the matching results
+  } else {
+
+    if (selectedCategory === "movies") {
+      searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(searchTerm)}`;
+    } else if (selectedCategory === "tv_shows") {
+      searchUrl = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${encodeURIComponent(searchTerm)}`;
+    } else if (selectedCategory === "actors") {
+      searchUrl = `https://api.themoviedb.org/3/search/person?api_key=${API_KEY}&query=${encodeURIComponent(searchTerm)}`;
+    }
+  }
+
+
+  //We do a get-request to our url and convert the answer to json-format
+  fetch(searchUrl)
+    .then((res) => res.json())
+    .then((data) => {
+
+      const topRatedMovies = data.results.slice(0, 10);
+
+      //console.log(json))
+      moviesContainer.innerHTML = '';
+
+      //Loops trough our movieobject from data results and creates a moviecard which we put in our movieconatiner
+      //If there is no result error message will appear
+      if (data.results.length === 0) {
+        const errorMessage = document.createElement('div');
+        errorMessage.textContent = "No movie found matching the description!";
+        moviesContainer.appendChild(errorMessage);
+
+      } else {
+
         data.results.forEach(media => {
           const movieCard = createMovieCard(media);
           moviesContainer.appendChild(movieCard);
-
+          console.log(data);
         });
-      })
-      .catch((err) => console.error("error:" + err));
-  }
+      }
+    })
+    .catch((err) => console.error("error:" + err));
+
+
+
+  //If no searchTerm is written error message will appear
+  const errorMessage = document.createElement('div');
+  errorMessage.textContent = "An error occurred!";
+  moviesContainer.appendChild(errorMessage);
+
+if (searchTerm === '') {
+  const errorMessage = document.createElement('div');
+  errorMessage.textContent = "Please enter a movie to search for";
+  moviesContainer.appendChild(errorMessage);
+}
+
 });
 
-//
+//Function that extracts title, creates the movie card in our div element,
+// adds css style and returns the movie card element
+function createMovieCard(media) {
+  const { title, name, backdrop_path, overview, release_date } = media;
+
+  const movieCard = document.createElement("div");
+  movieCard.classList.add("movie_item")
+
+  movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${backdrop_path}" class="movie_img_rounded">
+                          <h3>${title || name}</h3>
+                          <div class = "overview">${overview}</div>
+                          <h4>Release Date: ${release_date}</h4>
+                          `;
+
+  return movieCard;
+}
+
+//Not using this function for now
+/*
 async function fetchMovies() {
   try {
     const response = await fetch(searchUrl);
@@ -50,17 +114,5 @@ async function fetchMovies() {
   }
 
 }
-
-
-function createMovieCard(media) {
-  const { title, name, backdrop_path } = media;
-
-  const movieCard = document.createElement("div");
-  movieCard.classList.add("movie_item")
-
-  movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${backdrop_path}" class="movie_img_rounded">
-                          <div class = "title">${title || name}</div>`;
-  return movieCard;
-}
-
-fetchMovies();
+*/
+//fetchMovies();
