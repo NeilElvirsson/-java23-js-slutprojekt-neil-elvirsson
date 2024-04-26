643d55dd991a7a21ba1db41558c8c38bdf4409e1
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2024-04-26 15:03:10 +0200

    added some error handling

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ searchForm.addEventListener('submit', function (event) {
   //Gets the matching css choice in our input where the attibute name is set to category wich is marked with checked
   const selectedCategory = document.querySelector('input[name="category"]:checked').value;
   let searchUrl;
+  
 
   //If searchterm is empty and radio button movies or action is selected show top rated or popular
   if (searchTerm === '' && selectedCategory === "top_rated") {
@@ -26,7 +27,7 @@ searchForm.addEventListener('submit', function (event) {
     searchUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
 
     //if a searchterm is put in and radio button is choosen, show the matching results
-    //encodeURIComponent transform text so its matching the url standard. 
+    
   } else {
 
     if (selectedCategory === "movies") {
@@ -36,12 +37,17 @@ searchForm.addEventListener('submit', function (event) {
     }
   }
 
-  //We do a get-request to our url and convert the answer to json-format
+  //We do a get-request to our url and convert the answer to json-format, of response is ok
+  
   fetch(searchUrl)
-    .then((res) => res.json())
-    .then((data) => {
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return res.json();
+    })
 
-      //console.log(json))
+    .then((data) => {
       moviesContainer.innerHTML = '';
 
       //If there is no result error message will appear
@@ -61,12 +67,9 @@ searchForm.addEventListener('submit', function (event) {
 
       } else if (selectedCategory === "top_rated" || selectedCategory === "popular") {
 
-        //Loops trough our movieobject from data results and creates a moviecard which we put in our movieconatiner
-        //if selected category is top rated or popular slice gives us the first 10 objects
-        //Else, the data object gives us all the media in the result
-
+        //Loops trough our movieobject from data results and creates a moviecard 
         data.results.slice(0, 10).forEach(media => {
-          const ratedCard = createRatedCard(media);  // Create a topRated card, topRatedCard thats only show pic, title, releasedate
+          const ratedCard = createRatedCard(media); 
           moviesContainer.appendChild(ratedCard);
           console.log(data);
         });
@@ -90,74 +93,74 @@ searchForm.addEventListener('submit', function (event) {
 
       }
     })
-    .catch((err) => console.error("error:" + err));
-});
+    //Catch for error handling
+    .catch((err) => {
+      console.error("Error fetching data:" + err);
+      displayError("There was an error fetching data!")
+
+    });
 
 
 
-//Function for movies that extracts title, pic, overview and release date,  creates the movie card in our div element
+  //Function for movies that extracts title, pic, overview and release date,  creates the movie card in our div element
 
-function createMovieCard(media) {
-  const { title, name, poster_path, overview, release_date } = media;
+  function createMovieCard(media) {
+    const { title, name, poster_path, overview, release_date } = media;
 
-  const movieCard = document.createElement("div");
-  movieCard.classList.add("movie_item")
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie_item")
 
-  movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${poster_path}" class="movie_img_rounded">
+    movieCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${poster_path}" class="movie_img_rounded">
                           <h3>${title || name}</h3>
                           <div class = "overview">${overview}</div>
                           <h4>Release Date: ${release_date}</h4>
                           `;
 
-  return movieCard;
-}
+    return movieCard;
+  }
 
-//Function for actors that extracts title, pic and known for, creates the actor card in our div element
+  //Function for actors that extracts title, pic and known for, creates the actor card in our div element
 
-function createActorCard(media) {
-  const { name, known_for_department, profile_path, known_for } = media;
+  function createActorCard(media) {
+    const { name, known_for_department, profile_path, known_for } = media;
 
-  const actorCard = document.createElement("div");
-  actorCard.classList.add("actor_item")
-  console.log(known_for[1]);
-  // console.log(known_for_department);
-  // console.log(media);
-  let knownForList = '';
-  
-  known_for.forEach(item => {
-    if (item.title) {
-      knownForList += `<p>Movie: ${item.title}</p>`;
-    } else if (item.name) {
-      knownForList += `<p>TV: ${item.name}</p>`;
-    }
-  });
+    const actorCard = document.createElement("div");
+    actorCard.classList.add("actor_item")
 
-  //const knownForList = known_for.map(item => `<li>${item.title || item.name}</li>`).join('');
+    let knownForList = '';
 
-  actorCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w400/${profile_path}">
+    //if statment to check if item (known for) is movie or tv
+    known_for.forEach(item => {
+      if (item.title) {
+        knownForList += `<p>Movie: ${item.title}</p>`;
+      } else if (item.name) {
+        knownForList += `<p>TV: ${item.name}</p>`;
+      }
+    });
+
+    actorCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w400/${profile_path}">
                           <h3>${name}</h3>
                           <p>${known_for_department}</p>
                           <h4>Known for: </h4>
                           <ul>${knownForList}</ul> 
                           `;//Gör en foreach loop och hämta ut arrayn
-  return actorCard;
-}
+    return actorCard;
+  }
 
-//Function for rated and popular movies, extracts title, pic, and release date
+  //Function for rated and popular movies, extracts title, pic, and release date
 
-function createRatedCard(media) {
-  const {title, name, release_date, poster_path} = media;
+  function createRatedCard(media) {
+    const { title, name, release_date, poster_path } = media;
 
-  const ratedCard = document.createElement("div");
-  ratedCard.classList.add("movie_item");
+    const ratedCard = document.createElement("div");
+    ratedCard.classList.add("movie_item");
 
-  ratedCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${poster_path}" class="movie_img_rounded">
+    ratedCard.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${poster_path}" class="movie_img_rounded">
                           <h3>${title || name}</h3>
                           <h4>Release Date: ${release_date}</h4>
                           `;
+    return ratedCard;
 
-                          return ratedCard;
-
-}
-
+  }
+});
 
